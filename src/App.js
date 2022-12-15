94e4fc98da3151083cecd321c6503a74dc7393dd
nikhilmahanta5 <66936559+nikhilmahanta5@users.noreply.github.com>
2022-12-15 21:56:10 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {treeData} from "./components/TreeData";
+import Tree from "./components/Tree";
+import FamilyData from "./components/FamilyData";
+import Form from "./components/Form";
+import formJSON from '../src/components/Data.json'
+import AddFamilyMember from "./components/AddFamilyMember";
+import { useState } from "react";
+
 
 function App() {
+  const [selectedMember, setSelectedMember] = useState({})
+
+  const handleSelectedMemberChange = (data)=>{
+    setSelectedMember(data)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="web-body">
+        <div className="familyTree">
+          Family tree
+          <div className="familyTreeDetails">
+              <input type="search" name="" id="" />
+              <Tree handleSelectedMemberChange={handleSelectedMemberChange} data={formJSON} />
+           
+          </div>
+          <div className="buttons">
+            <div>
+              <button> Import JSON</button>
+              <button onClick={AddFamilyMember}>Add Family</button>
+            </div>
+            <div>
+              <button>Export JSON</button>
+              <button>Print Family Tree</button>
+            </div>
+          </div>
+        </div>
+        <div className="familyDetails">
+          Family Details
+        <FamilyData   data={selectedMember} />
+        {/* <Form/> */}
+          </div>
+      </div>
     </div>
   );
 }
